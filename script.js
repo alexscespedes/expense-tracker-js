d8aa2d36c8159e90945c072505252babd1624883
alexscespedes <alexandersenc99@gmail.com>
2025-04-12 13:11:42 -0400

    define the variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+
+const expenseForm = document.getElementById("expense-form");
+const expenseInput = document.getElementById("expense-input");
+const amountInput = document.getElementById("amount-input");
+const categoryInput = document.getElementById("category-input");
+const transactionList = document.getElementById("transaction-list");
+const totalExpense = document.getElementById("total-expense");
+const totalIncome = document.getElementById("total-income");
+const balance = document.getElementById("balance");
